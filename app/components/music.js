cbca05873c8d022e53ba04127f71d788ffa2019c
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-07-29 23:56:25 -0500

    Completed Music Component
    Completed adding functionality and styles to the Music component. Now scrolls and displays a song title, alongside blurring on the edges. The play button does not current switch to a pause button, that functionality can be added in later

--- app/components/music.js
+++ app/components/music.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+    isPlayCharacter: true,
+    musicButtonCharacter: "▶",
+
+    actions: {
+        changeMusicButton() {
+            // if(this.isPlayCharacter) {
+            //     musicButtonCharacter = "▶";
+            // }
+            // else {
+            //     musicButtonCharacter = "❙❙";
+            // }
+            // this.toggleProperty('isPlayCharacter');
+            this.musicButtonCharacter = "Hi";
+        }
+    }
+
+
 });
