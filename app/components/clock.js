192a8b054f098559c8adf261100a36f177bbc712
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-07-30 00:31:09 -0500

    Began Clock Component
    Began creating the clock component. Installed ember-cli-clock for assistance with scripting a consistent clock

--- app/components/clock.js
+++ app/components/clock.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+    clock: service("eu-clock"),
+
+    currentClockTime: computed('clock.date', function () {
+        var clockValue = "";
+        
+        clockValue = this.get('clock.date').getHours()
+                    + ":" + this.get('clock.date').getMinutes()
+                    + ":" + this.get('clock.date').getSeconds();
+
+        return clockValue;
+    })
+
 });
---
+++ app/services/eu-clock.js
@@ -0,0 +1,7 @@
+//Clock functionality and interval counter provided by ember-cli-clock
+//https://github.com/jerel/ember-cli-clock
+import Clock from 'ember-cli-clock/services/clock';
+
+export default Clock.extend({
+  interval: 1000
+});
