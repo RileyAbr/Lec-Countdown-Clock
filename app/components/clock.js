1f56b78c5298c820f2ffec06b8471656e4ecd3b0
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-07-30 21:32:03 -0500

    Added Time and Date Collection
    The clock.js file now controls and validates the time and date to display in the clock component. The time of day, season, and year can all be displayed. Week "Four" is currently a placeholder

--- app/components/clock.js
+++ app/components/clock.js
@@ -8,12 +8,64 @@ export default Component.extend({
 
     currentClockTime: computed('clock.date', function () {
         var clockValue = "";
-        
-        clockValue = this.get('clock.date').getHours()
-                    + ":" + this.get('clock.date').getMinutes()
-                    + ":" + this.get('clock.date').getSeconds();
+
+        var hours = this.get('clock.date').getHours();
+        var minutes = this.get('clock.date').getMinutes();
+        var seconds = this.get('clock.date').getSeconds();
+
+        if(hours > 12) {
+            hours = hours - 12;
+        }
+        if(hours < 10) {
+            hours = "0" + hours;
+        }
+        if(minutes <10) {
+            minutes = "0" + minutes;
+        }
+        if(seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        clockValue = hours
+                    + ":" + minutes
+                    //+ ":" + seconds
+                    ;
 
         return clockValue;
+    }),
+
+    currentYear: computed('clock.date', function () {
+        var yearValue = "";
+
+        var year = this.get('clock.date').getFullYear();
+
+        yearValue = year.toString().substr(2, 2);
+
+        return yearValue;
+    }),
+
+    currentMonth: computed('clock.date', function () {
+        var monthValue = "";
+
+        var month = this.get('clock.date').getMonth() + 1; //Month value starts at 0
+
+        if(month > 2
+            && month < 6) {
+            monthValue = "SPRING";
+        }
+        else if(month > 5
+            && month < 9) {
+            monthValue = "SUMMER";
+        }
+        else if(month > 8
+                && month < 12) {
+            monthValue = "AUTUMN";
+        }
+        else {
+            monthValue = "WINTER";
+        }
+
+        return monthValue;
     })
 
 });
