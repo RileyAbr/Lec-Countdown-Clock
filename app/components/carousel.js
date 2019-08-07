1d4b68d51c6324e2a510735534d6cbb4b860e43f
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2019-08-06 23:28:16 -0500

    Began assembling Sponsors component

--- app/components/carousel.js
+++ app/components/carousel.js
@@ -1,4 +1,7 @@
 import Component from '@ember/component';
 
+// Useful info for connecting to the unofficial LoL Competitive API: https://gist.github.com/levi/e7e5e808ac0119e154ce
+
 export default Component.extend({
+    arrayOFgroup:['apple','pineapple','banana']
 });
--- app/components/sponsors.js
+++ app/components/sponsors.js
@@ -1,4 +1,32 @@
 import Component from '@ember/component';
+import { later } from '@ember/runloop';
+
+// later(partnerType, function() {
+//     // code here will execute within a RunLoop in about 500ms with this == myContext
+//     partnerType = "SUB";
+// }, 500);
+
 
 export default Component.extend({
+    partnerType: "Y",
+
+    // checkPartnerType: function () {
+    //     let self = this;
+
+    //     this.set('partnerType', "MAIN");
+
+    //     // Ember.run.later(this, function () {
+    //     //     this.partnerType = "SUB";
+    //     //     return this.partnerType;
+    //     // }, 1000);
+
+    //     return this.partnerType;
+    // },
+
+    mainSponsors: ['KIA', 'ALIENWARE'],
+    subSponsors: ['Footlocker'],
+    mainLogos: ['assets/Kia.png','assets/Alienware.png']
+
+
 });
+
